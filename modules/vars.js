cdb411f6a614f07635aa28348b2eef9599314c5d
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-01-28 12:35:11 -0500

    Initial commit

---
+++ modules/vars.js
@@ -0,0 +1,11 @@
+import { config } from 'dotenv';
+config();
+
+const { DB_USER, DB_PASSWORD, DB_ADDRESS, DB_CLUSTER } = process.env;
+
+export default {
+  DB_USER,
+  DB_PASSWORD,
+  DB_ADDRESS,
+  DB_CLUSTER,
+};
---
+++ server.js
@@ -0,0 +1,4 @@
+import vars from './modules/vars.js';
+const { DB_CLUSTER } = vars;
+
+console.log(`DB_CLUSTER: ${DB_CLUSTER}`);
