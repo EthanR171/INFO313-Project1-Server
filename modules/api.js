d7b0b96f990083ed62945c46e7663b2de1fb2645
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-02-04 15:19:45 -0500

    Updated GET endpoint to accept request query strings
    So far we've seen we can send data in the body of a POST request (but also a PUT, DELETE, and any non-GET methods), through a route path variable, and now we'll see one final way data can be send through requests: query strings.
    
    Though any method can use query strings, it's most commonly associated with GET requests because these requests can't have a body.
    
    We have a way to get all users at the moment, but no way to get a specific user. Let's update our endpoint to add to the search criteria from the query strings.
    
    If we console.log the value of request.query, we'll see anything that gets added to the query strings of the request on the format ?name=value&other_name=other_value where the ?, the = and the & are delimiting special characters.

--- modules/api.js
+++ modules/api.js
@@ -1,5 +1,5 @@
 import * as db from './db.js';
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
 app.use(express.json()); // middleware to parse JSON bodies (allows us to use request.body)
@@ -7,9 +7,20 @@ app.use(express.json()); // middleware to parse JSON bodies (allows us to use re
 const configure = (client, vars) => {
   const { DB_NAME } = vars;
 
-  app.get('/api/users', async (_request, response) => {
+  app.get('/api/users', async (request, response) => {
     try {
-      let result = await db.findDocuments(client, DB_NAME, 'users', {});
+      //console.log(request.query);
+      let criteria = {}; // this will hold the formated contents of the request query strings if any are given
+      for (let [key, value] of Object.entries(request.query)) {
+        criteria[key] = {
+          $regex: value,
+          $options: 'i', // ingore case
+        };
+      }
+
+      console.log(criteria);
+
+      let result = await db.findDocuments(client, DB_NAME, 'users', criteria);
       response.status(200).send(result);
     } catch (e) {
       console.error(e); // print on the server
@@ -25,6 +36,23 @@ const configure = (client, vars) => {
         'users',
         request.body
       );
+      response.status(200).send(result);
+    } catch (e) {
+      console.error(e);
+      response.status(500).send(`${e}`);
+    }
+  });
+
+  // note: anything after the colon is a variable contained in the request object
+  // we need the middleware to parse the JSON as defined by app.use(express.json())
+  app.delete('/api/users/:email', async (request, response) => {
+    try {
+      let result = await db.deleteDocument(client, DB_NAME, 'users', {
+        email: {
+          $regex: request.params.email,
+          $options: 'i',
+        },
+      });
       response.status(200).send(result);
     } catch (e) {
       console.error(e);
